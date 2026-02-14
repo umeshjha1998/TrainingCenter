ef4641acecf821674efea0d55901c5afde0d37bc
UMESH JHA <33800467+umeshjha1998@users.noreply.github.com>
2026-02-15 01:01:12 +0530

    Fix add student add new course and generate certificate

--- src/pages/admin/ManageStudents.jsx
+++ src/pages/admin/ManageStudents.jsx
@@ -109,7 +109,7 @@ export default function ManageStudents() {
                 </table>
             </div>
             {isModalOpen && (
-                <div className="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                <div className="fixed inset-0 z-[100] overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
                     <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                         <div className="fixed inset-0 bg-slate-900 bg-opacity-75 transition-opacity" onClick={() => setIsModalOpen(false)} aria-hidden="true"></div>
                         <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
